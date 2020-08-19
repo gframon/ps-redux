8669487ed3910c201af0aae224a0b67ec9ac1357
Ramon Gomez <ramon.gomez@noaa.gov>
2020-08-19 15:17:45 -0400

    Added Authors functionality.

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,18 +1,28 @@
 import React from "react";
 import { connect } from "react-redux";
 import * as courseActions from "../../redux/actions/courseActions";
+import * as authorActions from "../../redux/actions/authorActions";
 import PropTypes from "prop-types";
 import { bindActionCreators } from "redux";
 import CourseList from "./CourseList";
 
 class CoursesPage extends React.Component {
   componentDidMount() {
-    const { actions } = this.props;
+    const { actions, authors, courses } = this.props;
 
-    actions.loadCourses().catch((error) => {
-      alert(`Loading courses failed.  ${error}`);
-    });
+    if (courses.length === 0) {
+      actions.loadCourses().catch((error) => {
+        alert(`Loading courses failed.  ${error}`);
+      });
+    }
+
+    if (authors.length === 0) {
+      actions.loadAuthors().catch((error) => {
+        alert(`Loading authors failed.  ${error}`);
+      });
+    }
   }
+
   render() {
     return (
       <>
@@ -24,19 +34,33 @@ class CoursesPage extends React.Component {
 }
 
 CoursesPage.propTypes = {
+  authors: PropTypes.array.isRequired,
   courses: PropTypes.array.isRequired,
   actions: PropTypes.object.isRequired,
 };
 
 function mapStateToProps(state) {
   return {
-    courses: state.courses,
+    courses:
+      state.authors.length === 0
+        ? []
+        : state.courses.map((course) => {
+            return {
+              ...course,
+              authorName: state.authors.find((a) => a.id === course.authorId)
+                .name,
+            };
+          }),
+    authors: state.authors,
   };
 }
 
 function mapDispatchToProps(dispatch) {
   return {
-    actions: bindActionCreators(courseActions, dispatch),
+    actions: {
+      loadCourses: bindActionCreators(courseActions.loadCourses, dispatch),
+      loadAuthors: bindActionCreators(authorActions.loadAuthors, dispatch),
+    },
   };
 }
 
