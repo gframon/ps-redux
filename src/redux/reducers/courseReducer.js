61e5c0b173fb3d63df1bfc9c3edf960471566197
Ramon Gomez <ramon.gomez@noaa.gov>
2020-08-19 08:34:51 -0400

    Continue implementing Redux.

---
+++ src/redux/reducers/courseReducer.js
@@ -0,0 +1,10 @@
+import actionTypes from "../actions/actionTypes";
+
+export default function courseReducer(state = [], action) {
+  switch (action.type) {
+    case actionTypes.CREATE_COURSE:
+      return [...state, { ...action.course }];
+    default:
+      return state;
+  }
+}
